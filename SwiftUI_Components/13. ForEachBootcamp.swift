1185f9e4d3a12f1d80c2aae3ea0425ebb627cd5b
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-03-29 07:50:48 +0200

    scroolViews

---
+++ SwiftUI_Components/13. ForEachBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ForEachBootcamp.swift
+//  SwiftUI_Components
+//
+//  Created by Manyuchi, Carrington C on 2025/03/28.
+//
+
+import SwiftUI
+
+struct ForEachBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEachBootcamp()
+}
---
+++ SwiftUI_Components/ScrollViewsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewsBootcamp.swift
+//  SwiftUI_Components
+//
+//  Created by Manyuchi, Carrington C on 2025/03/28.
+//
+
+import SwiftUI
+
+struct ScrollViewsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewsBootcamp()
+}
